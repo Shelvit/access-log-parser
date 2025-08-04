bd7b1ddcd921e89ef27ede88edae05465de04e54
VSheloukhov <VSheloukhov@inno.tech>
2025-08-04 11:17:58 +0300

    Add string analyze

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
+import java.io.*;
 import java.util.Scanner;
-import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,21 +19,56 @@ public class Main {
             boolean fileExists = file.exists();
             boolean isFile = file.isFile();
 
-            if (!fileExists || !isFile) {
-                if (!fileExists) {
-                    System.out.println("Файл не существует.");
-                }
-                if (!isFile) {
-                    System.out.println("Указанный путь является папкой, а не файлом.");
-                }
+            if (!fileExists) {
+                System.out.println("Файл не существует.");
+                continue;
+            }
+
+            if (!isFile) {
+                System.out.println("Указанный путь является папкой, а не файлом.");
                 continue;
             }
 
             validFileCount++;
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + validFileCount);
+
+            try {
+                FileReader fileReader = new FileReader(filePath);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+                int totalLines = 0;
+                int maxLength = Integer.MIN_VALUE;
+                int minLength = Integer.MAX_VALUE;
+
+                while ((line = reader.readLine()) != null) {
+                    totalLines++;
+
+                    int length = line.length();
+
+                    if (length > 1024) {
+                        throw new RuntimeException("Встречена строка длиной более 1024 символов.");
+                    }
+
+                    if (length > maxLength) {
+                        maxLength = length;
+                    }
+
+                    if (length < minLength) {
+                        minLength = length;
+                    }
+                }
+
+                System.out.println("Общее количество строк в файле: " + totalLines);
+                System.out.println("Длина самой длинной строки в файле: " + maxLength);
+                System.out.println("Длина самой короткой строки в файле: " + minLength);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
         }
 
-    }
 
+    }
 }
